81b1d106ae32a58aeda9bf4ff248dba3258c8c3b
17252AleksaMilojkovic <aleksa.milojkovic.bs@elfak.rs>
2022-05-29 20:58:01 +0200

    Dodat Forgot Password Fragment

---
+++ CampingApp/app/src/main/java/elfak/mosis/campingapp/FragmentForgotPassword.kt
@@ -0,0 +1,35 @@
+package elfak.mosis.campingapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import elfak.mosis.campingapp.databinding.FragmentForgotPasswordBinding
+
+
+class FragmentForgotPassword : Fragment()
+{
+
+    private lateinit var binding:FragmentForgotPasswordBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+        binding.imageViewBackButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View?
+    {
+        binding = FragmentForgotPasswordBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+
+}
