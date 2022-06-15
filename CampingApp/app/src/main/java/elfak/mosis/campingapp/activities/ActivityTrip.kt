7aaf49db958270f170e55cdac5852036be2f1d75
MatijaNikolic17312 <matija.nikolic.bs@elfak.rs>
2022-06-15 21:46:59 +0200

    I kao imamo navbar...

---
+++ CampingApp/app/src/main/java/elfak/mosis/campingapp/activities/ActivityTrip.kt
@@ -0,0 +1,12 @@
+package elfak.mosis.campingapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import elfak.mosis.campingapp.R
+
+class ActivityTrip : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trip)
+    }
+}
