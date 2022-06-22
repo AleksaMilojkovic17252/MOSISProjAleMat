6598dec666140b7b30347c18487bdfbb82f3f2db
MatijaNikolic17312 <matija.nikolic.bs@elfak.rs>
2022-06-22 22:12:16 +0200

    Uradjeno kreiranje request za dodavanje prijatelja

--- CampingApp/app/src/main/java/elfak/mosis/campingapp/fragments/FragmentAddTeammate.kt
+++ CampingApp/app/src/main/java/elfak/mosis/campingapp/fragments/FragmentAddTeammate.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import elfak.mosis.campingapp.R
 import elfak.mosis.campingapp.databinding.FragmentAddTeammateBinding
 
@@ -15,6 +19,58 @@ class FragmentAddTeammate : Fragment()
     {
         binding = FragmentAddTeammateBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSendInvite.setOnClickListener {
+            var refFriend = binding.editTextFriendID.text.toString()
+            sendTeammateRequest(refFriend)
+        }
+    }
+
+    private fun sendTeammateRequest(refFriend: String)
+    {
+        if (android.util.Patterns.EMAIL_ADDRESS.matcher(refFriend).matches())
+        {
+            Firebase.firestore
+                .collection(getString(R.string.db_coll_users))
+                .whereEqualTo("email", refFriend)
+                .get()
+                .addOnSuccessListener {
+                    for (doc in it)
+                    {
+                        var zaDodati = hashMapOf(
+                            "from" to Firebase.auth.currentUser!!.uid,
+                            "to" to doc.id
+                        )
+
+                        Firebase.firestore
+                            .collection(getString(R.string.db_coll_req))
+                            .add(zaDodati)
+                            .addOnSuccessListener {
+                                Toast.makeText(requireContext(), "Request sent!", Toast.LENGTH_SHORT).show()
+                            }
+                        break
+                    }
+                }
+        }
+        else
+        {
+            var zaDodati = hashMapOf(
+                "from" to Firebase.auth.currentUser!!.uid,
+                "to" to refFriend
+            )
+
+            Firebase.firestore
+                .collection(getString(R.string.db_coll_req))
+                .add(zaDodati)
+                .addOnSuccessListener {
+                    Toast.makeText(requireContext(), "Request sent!", Toast.LENGTH_SHORT).show()
+                }
+        }
     }
 
 
