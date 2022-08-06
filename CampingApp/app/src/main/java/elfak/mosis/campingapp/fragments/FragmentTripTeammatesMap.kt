80a7954ca07a723df0a08cf5ffccfd21a5e6f97b
17252AleksaMilojkovic <aleksa.milojkovic.bs@elfak.rs>
2022-08-06 17:07:19 +0200

    Uzima se trenutna lokacija korisnika

--- CampingApp/app/src/main/java/elfak/mosis/campingapp/fragments/FragmentTripTeammatesMap.kt
+++ CampingApp/app/src/main/java/elfak/mosis/campingapp/fragments/FragmentTripTeammatesMap.kt
@@ -1,24 +1,33 @@
 package elfak.mosis.campingapp.fragments
 
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import elfak.mosis.campingapp.R
 import elfak.mosis.campingapp.databinding.FragmentTripTeammatesMapBinding
+import org.osmdroid.api.IMapController
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 
 class FragmentTripTeammatesMap : Fragment()
 {
     lateinit var binding: FragmentTripTeammatesMapBinding
     private lateinit var mapa: MapView
+    private lateinit var myLocationOverlay: MyLocationNewOverlay
+    private lateinit var mapController: IMapController
+    val PERMISSION_ACCESS_FINE_LOCATION = 1
 
     override fun onResume() {
         super.onResume()
@@ -42,16 +51,44 @@ class FragmentTripTeammatesMap : Fragment()
         mapa = binding.osmMapView
 
 
+        if(ActivityCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(requireActivity(),android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+        else
+        {
+            setMyLocationOverlay()
+        }
         mapa.setMultiTouchControls(true)
-        mapa.controller.setZoom(8.5)
-        mapa.controller.setCenter(GeoPoint(44.0333, 20.8))
+
 
         binding.teammatesTeammates.setOnClickListener{
             findNavController().navigate(R.id.action_fragmentTripTeammates_to_fragmentTripTeammates2)
         }
     }
 
+    private fun setMyLocationOverlay(){
+        mapa.controller.setZoom(15)
+        mapa.controller.setCenter(GeoPoint(44.0333, 20.8))
+        myLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(activity),mapa)
+        myLocationOverlay.enableMyLocation()
+        mapa.overlays.add(myLocationOverlay)
+        mapController = mapa.controller;
+        if(mapController != null)
+        {
+            myLocationOverlay.enableFollowLocation()
+        }
 
+    }
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()){
+        isGranted:Boolean->
+        if(isGranted){
+            setMyLocationOverlay()
+
+        }
+    }
 
     override fun onPause()
     {
